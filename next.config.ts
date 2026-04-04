58c9f364a7f40aadbcffe9c8516e07ce0bd3020c
saikrishnayadav764 <106056182+saikrishnayadav764@users.noreply.github.com>
2026-04-05 01:23:18 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  // @ts-ignore - Next.js 15 property
+  allowedDevOrigins: ["0.0.0.0", "localhost"]
+} as any;
 
 export default nextConfig;
