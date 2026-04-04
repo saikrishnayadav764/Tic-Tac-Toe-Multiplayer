d1bc1722c30e8f8d5c72f370dec70c4396c556da
saikrishnayadav764 <106056182+saikrishnayadav764@users.noreply.github.com>
2026-04-05 01:26:36 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // @ts-ignore - Next.js 15 property
-  allowedDevOrigins: ["0.0.0.0", "localhost"]
+  allowedDevOrigins: ["0.0.0.0", "localhost", "tic-tac-toe-multiplayer-ruby.vercel.app"]
 } as any;
 
 export default nextConfig;
